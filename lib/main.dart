3e88f2e148cd2ed55e0ffd7835bcc2bef0388ea4
rafiramzi <79634000+rafiramzi@users.noreply.github.com>
2024-01-20 10:55:32 +0700

    test
    test

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:my_chat_app/utils/constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:my_chat_app/pages/splash_page.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+
+  // Now that dotenv is loaded, you can access the environment variables
+  await Supabase.initialize(
+    url: dotenv.env['DB_URL']!,
+    anonKey: dotenv.env['ANON_KEY']!,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Chat App',
+      theme: appTheme,
+      home: const SplashPage(),
+    );
+  }
+}
